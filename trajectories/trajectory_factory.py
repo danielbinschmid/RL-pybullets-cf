0514ea5e0b77b48174c702b2c611d7bf44a4f17c
binbash <danielbinschmid@outlook.de>
2024-01-20 20:05:24 +0100

    polynomial trajectory pid

--- trajectories/trajectory_factory.py
+++ trajectories/trajectory_factory.py
@@ -3,6 +3,49 @@ from .square_linear_trajectory import SquareLinearTrajectory
 from .polynomial_discretized_trajectory import PolynomialDiscretizedTrajectory
 from .waypoint import Waypoint
 from typing import List
+import numpy as np 
+from typing import Optional
+
+class WAYPOINT_BOOTH:
+    T_WAYPOINTS_POLY = [
+         Waypoint(
+              coordinate=np.asarray([0, 0, 0]),
+              timestamp=0
+         ),
+         Waypoint(
+              coordinate=np.asarray([0, 1, 0.25]),
+              timestamp=2
+         ),
+         Waypoint(
+              coordinate=np.asarray([1, 1, 0.5]),
+              timestamp=4
+         ),
+         Waypoint(
+              coordinate=np.asarray([1, 0, 0.75]),
+              timestamp=8
+         ),
+         Waypoint(
+              coordinate=np.asarray([0, 0, 1]),
+              timestamp=10
+         ), 
+         Waypoint(
+              coordinate=np.asarray([1,1,1]),
+              timestamp=12
+         ),
+         Waypoint(
+              coordinate=np.asarray([1, 0, 0.75]),
+              timestamp=14
+         ),
+         Waypoint(
+              coordinate=np.asarray([1, 1, 0.5]),
+              timestamp=16
+         ),
+         Waypoint(
+              coordinate=np.asarray([0, 0, 0]),
+              timestamp=18
+         ),
+    ]
+
 
 class TrajectoryFactory:
     """
@@ -16,7 +59,13 @@ class TrajectoryFactory:
         )
 
     @classmethod
-    def get_pol_discretized_trajectory(cls, t_waypoints: List[Waypoint], n_points_discretization_level: int) -> PolynomialDiscretizedTrajectory:
+    def get_pol_discretized_trajectory(cls, t_waypoints: Optional[List[Waypoint]]=None, n_points_discretization_level: Optional[int] = None) -> PolynomialDiscretizedTrajectory:
+        
+        if t_waypoints is None:
+            t_waypoints = WAYPOINT_BOOTH.T_WAYPOINTS_POLY
+        if n_points_discretization_level is None: 
+            n_points_discretization_level = 100
+        
         return PolynomialDiscretizedTrajectory(
             t_waypoints,
             n_points_discretization_level
