3b0f06bec170397f4e8047ceed899e563b57550e
strakam <strakammm@gmail.com>
2024-01-23 17:09:18 +0100

    remove simple follower

--- agents/follower.py
+++ agents/follower.py
@@ -42,7 +42,7 @@ DEFAULT_OUTPUT_FOLDER = 'results'
 DEFAULT_COLAB = False
 
 DEFAULT_OBS = ObservationType('kin') # 'kin' or 'rgb'
-DEFAULT_ACT = ActionType('one_d_rpm') # 'rpm' or 'pid' or 'vel' or 'one_d_rpm' or 'one_d_pid'
+DEFAULT_ACT = ActionType('rpm') # 'rpm' or 'pid' or 'vel' or 'one_d_rpm' or 'one_d_pid'
 DEFAULT_AGENTS = 2
 DEFAULT_MA = False
 DEFAULT_TIMESTEPS = 20000
