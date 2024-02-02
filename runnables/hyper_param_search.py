9eb8b72e09eb6d41d36e883c058f6200258fb9ea
strakam <strakammm@gmail.com>
2024-02-02 17:24:20 +0100

    hp

--- runnables/hyper_param_search.py
+++ runnables/hyper_param_search.py
@@ -7,10 +7,10 @@ from concurrent.futures import ProcessPoolExecutor
 param_grid = {
     'waypoint_buffer_size': [3,4],
     'k_p' : [1, 2],
-    # 'k_wp' : [3, 7],
-    # 'k_s' : [0.1, 0.2],
-    # 'max_reward_distance': [0.1, 0.2],
-    # 'waypoint_dist_tol': [0.05, 0.12],
+    'k_wp' : [3, 7],
+    'k_s' : [0.1, 0.2],
+    'max_reward_distance': [0.1, 0.2],
+    'waypoint_dist_tol': [0.05, 0.12],
 }
 
 # Generate all combinations of parameters
