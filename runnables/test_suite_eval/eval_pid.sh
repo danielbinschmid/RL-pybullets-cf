e194c7001a1192988e7223ba1a5843db091f865a
binbash <danielbinschmid@outlook.de>
2024-02-12 12:00:47 +0100

    merged eval to single bash script

--- runnables/test_suite_eval/eval_pid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for level in {5..50}
-do
-  python pid.py --discr_level $level --eval_set "--eval_set "./test_suite_eval/eval-v0_n-ctrl-points-3_n-tracks-1000_2024-02-12_10:59:21_d689e67e-4179-4764-a474-e5f3237a239d"
-done
--- runnables/test_suite_eval/eval_rl.sh
+++ runnables/test_suite_eval/eval_rl.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for level in {5..50}
+for level in {5..20}
 do
   python C_trajectory_following.py --output_folder "../../checkpointed_models/" --eval_set "./test_suite_eval/eval-v0_n-ctrl-points-3_n-tracks-1000_2024-02-12_10:59:21_d689e67e-4179-4764-a474-e5f3237a239d" --discr_level $level
+  python pid.py --discr_level $level --eval_set "./test_suite_eval/eval-v0_n-ctrl-points-3_n-tracks-1000_2024-02-12_10:59:21_d689e67e-4179-4764-a474-e5f3237a239d"
+
 done
