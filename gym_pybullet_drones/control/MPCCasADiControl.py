00df752b5263b6e10f5bd65dff59d7d61d275607
Daniel He <he.daniel@proton.me>
2024-02-16 10:39:29 +0100

    test: Tested MPC Controller with the following weighting matrices:
    # Care 3x less about the reference input and velocities of the drone
    Q = diagcat(1, 1, 1, 0.2, 0.2, 0.1666, 0, 0, 0, 0, 0, 0)
    R = diagcat(0.1, 0.1, 0.1, 0.2666)
    
    Test result:
    # DEFAULT_DISCR_LEVEL = 10 -> goes off track after 3 points
    # DEFAULT_DISCR_LEVEL = 30 -> finishes 1st track with lots of mistakes
    # DEFAULT_DISCR_LEVEL = 50 -> Performs actually quite well with minor mistakes
    # DEFAULT_DISCR_LEVEL = 100 -> Performs actually quite well, but goes off track midway
    # DEFAULT_DISCR_LEVEL = 200 -> Performs actually quite well with minor mistakes. A little slower. Misses the last goal position (0,0,0) though
    
    fix: Also found about multiplier mistake before. It was actually 6x less and not 3x

--- gym_pybullet_drones/control/MPCCasADiControl.py
+++ gym_pybullet_drones/control/MPCCasADiControl.py
@@ -200,7 +200,7 @@ class MPCCasADiControl(BaseControl):
         R = diagcat(0, 0, 0, 0)
         # -> Test result: Drone circles around a reference point indefinitely and doesn't reach it'''
 
-        # Care 3x less about  and velocities and do not care about the reference input of the drone
+        '''# Care 6x (Correction: was 3x) less about  and velocities and do not care about the reference input of the drone
         Q = diagcat(1, 1, 1, 0.1, 0.1, 0.1666, 0, 0, 0, 0, 0, 0)
         R = diagcat(0, 0, 0, 0)
         # -> Test result:
@@ -208,10 +208,30 @@ class MPCCasADiControl(BaseControl):
         # DEFAULT_DISCR_LEVEL = 20 -> goes off track after 2 points
         # DEFAULT_DISCR_LEVEL = 30 -> goes off track after 7-8 points
         # DEFAULT_DISCR_LEVEL = 40 -> goes off track after 4 points
+        # DEFAULT_DISCR_LEVEL = 50 -> goes off track after 4 points'''
+
+        # Care 3x less about and velocities and reference input of the drone
+        # Reference Weighting matrices from the paper
+        #Q = diagcat(1, 1, 1, 0.6, 0.6, 1, 0, 0, 0, 0, 0, 0)
+        #R = diagcat(0.3, 0.3, 0.3, 0.8)
+
+
+        Q = diagcat(1, 1, 1, 0.1, 0.1, 0.1666, 0, 0, 0, 0, 0, 0) #!! Correction this is imbalanced weighting! This is 6x less
+        # It should be Q = diagcat(1, 1, 1, 0.2, 0.2, 0.1666, 0, 0, 0, 0, 0, 0)
+        R = diagcat(0.1, 0.1, 0.1, 0.2666) #
+        # DEFAULT_DISCR_LEVEL = 10 -> goes off track after 1 points
+        # DEFAULT_DISCR_LEVEL = 30 -> goes off track after 10 points
         # DEFAULT_DISCR_LEVEL = 50 -> goes off track after 4 points
 
-
-        #R = diagcat(0.1, 0.1, 0.1, 0.2666)
+        # Care 3x less about and velocities and reference input of the drone
+        Q = diagcat(1, 1, 1, 0.2, 0.2, 0.1666, 0, 0, 0, 0, 0, 0)
+        R = diagcat(0.1, 0.1, 0.1, 0.2666)
+        # DEFAULT_DISCR_LEVEL = 10 -> goes off track after 3 points
+        # DEFAULT_DISCR_LEVEL = 30 -> finishes 1st track with lots of mistakes
+        # DEFAULT_DISCR_LEVEL = 50 -> Performs actually quite well with minor mistakes
+        # DEFAULT_DISCR_LEVEL = 100 -> Performs actually quite well, but goes off track midway
+        # DEFAULT_DISCR_LEVEL = 200 -> Performs actually quite well with minor mistakes. A little slower. Misses
+        # the last goal position (0,0,0) though
 
         x_init = P[0:Nx]
         g = X[:, 0] - P[0:Nx]  # initial condition constraints
